54ec25d84ed34990b88e543c9f15168acf8310a3
AVGVSTVS96 <bassim101@gmail.com>
2024-05-08 15:10:43 -0400

    add astro-icon and tailwind typography, update tailwind config. add blogTimeline component, add linkCaret.svg, add accent variables to css and remove datae-theme variables

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,8 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
-
 import tailwind from "@astrojs/tailwind";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
@@ -11,5 +12,6 @@ export default defineConfig({
     tailwind({
       applyBaseStyles: false,
     }),
+    icon(),
   ],
 });
