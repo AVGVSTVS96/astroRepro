04e36351426e35e823f16f8b529276e101ce6730
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-05-09 23:50:49 -0400

    Remove unused sharpImageService

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,4 @@
-import { defineConfig, sharpImageService } from "astro/config";
+import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
