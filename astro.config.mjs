38d5d623d6917f9669ccc67cbb8802d101a7af4d
AVGVSTVS96 <122117267+AVGVSTVS96@users.noreply.github.com>
2024-05-09 23:44:37 -0400

    Update shiki theme, add `Meta.astro`, add blog post

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
+  markdown: {
+    shikiConfig: {
+      theme: "vesper",
+    },
+  },
   output: "hybrid",
   adapter: cloudflare({ imageService: "compile" }),
   integrations: [
